50ce2c711b2e7b765f0bc13dab106f88f9d0c2e0
Daniel Ayala <dannyboy3385@gmail.com>
2020-09-17 11:43:06 -0700

    Updates

---
+++ precache-manifest.c6c40e89449f74ba89ef87d856a899f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a73f9ee3c46d924a56ca8e82e2c4e3f5",
+    "url": "/DaniBoie/React002/index.html"
+  },
+  {
+    "revision": "0ed22d47e5e3c806c772",
+    "url": "/DaniBoie/React002/static/js/2.65ea254c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DaniBoie/React002/static/js/2.65ea254c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffe5e10a71a4361f62f1",
+    "url": "/DaniBoie/React002/static/js/main.26c3932d.chunk.js"
+  },
+  {
+    "revision": "8dd606ba5607d5594ed1",
+    "url": "/DaniBoie/React002/static/js/runtime-main.018a8d3c.js"
+  }
+]);
